a3310685272851058d902a98d0e292e0a0a1d2bf
Robson Júnior Schultz Dias <126567117+RobsonSchultz@users.noreply.github.com>
2023-05-24 10:42:46 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function converterTemp() {
-    var tempInput = document.getElementById('tempRange').value;
+    var body = document.querySelector('body');
+    var tempInput = parseFloat(document.getElementById('tempRange').value);
     document.getElementById('tempSpan').innerHTML = tempInput;
 
     var tempResultados = document.getElementById('resultados');
@@ -11,31 +12,42 @@ function converterTemp() {
     var imagem = document.getElementById('gato');
     var desc = document.getElementById('descricao');
 
-    if (tempInput > 40) {
-        document.body.style.backgroundColor = '#cf0a00';
+    if (tempInput > 152) {
         imagem.src = 'IMGs/gato_mucho_calor.gif'
         desc.innerHTML = 'O ventilador não é mais o suficiente. ABAIXE A TEMPERATURA!!!'
     }
-    else if (tempInput <= 40 && tempInput > 30) {
-        document.body.style.backgroundColor = '#ff7770';
+    else if (tempInput <= 151 && tempInput > 53) {
         imagem.src = 'IMGs/gato_poco_calor.jpg'
         desc.innerHTML = 'O gatinho sente um pouco de calor, mas se esfria com seu ventilador.'
     }
-    else if (tempInput <= 30 && tempInput > 20) {
-        document.body.style.backgroundColor = '#FFFFFF';
+    else if (tempInput <= 52 && tempInput > -52) {
         imagem.src = 'IMGs/gato_suavidad.png'
         desc.innerHTML = 'A temperatura está amena, o gatinho aprova a temperatura.'
     }
-    else if (tempInput <= 20 && tempInput > 10) {
-        document.body.style.backgroundColor = '#7dc7ff';
+    else if (tempInput <= -53 && tempInput > -151) {
         imagem.src = 'IMGs/gato_poco_frio.png'
         desc.innerHTML = 'O gatinho sente um pouco de frio, mas se esquenta com sua cobertinha.'
     }
     else {
-        document.body.style.backgroundColor = '#0091ff';
         imagem.src = 'IMGs/gato_mucho_frio.jpg'
         desc.innerHTML = 'O gatinho está congelando. AUMENTE A TEMPERATURA!!!'
     }
+
+    if (tempInput > 0) {
+        var red = 255;
+        var green = 255 - tempInput;
+        var blue = 255 - tempInput;
+        var alpha = 0.9
+
+    }else {
+        var red = 255 + tempInput;
+        var green = 255 + (tempInput / 2);
+        var blue = 255;
+        var alpha = 0.9
+    }
+
+    body.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, ${alpha})`
+
 
     tempResultados.innerHTML += 'Celsius: ' + tempInput + 'º Fahrenheit: ' + tempFahrenheit + 'º Kelvin: ' + tempKelvin + 'º';
 }
